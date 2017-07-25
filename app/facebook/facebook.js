7591cee18eaa95c636c18eff74c701b06c0cb09a
Pogrebnyak Alex <pogrebnyakalexx@gmail.com>
2017-07-25 19:11:14 +0300

    Fix permissions at

--- app/facebook/facebook.js
+++ app/facebook/facebook.js
@@ -11,7 +11,7 @@ angular.module('ngSocial.facebook', ['ngRoute','ngFacebook'])
 
 .config( function( $facebookProvider ) {
   $facebookProvider.setAppId('450853485276464');
-  $facebookProvider.setPermissions("email","public_profile","user_posts","publish_actions","user_photos");  
+  $facebookProvider.setPermissions('email, public_profile, user_posts, publish_actions, user_photos');  
 })   
 
 .run( function( $rootScope ) {
