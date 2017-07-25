37c4798c0716d24ac3b51a916b071ea54450baaf
Pogrebnyak Alex <pogrebnyakalexx@gmail.com>
2017-07-25 15:02:40 +0300

    Fix 3: Add user details

--- app/facebook/facebook.js
+++ app/facebook/facebook.js
@@ -43,7 +43,7 @@ angular.module('ngSocial.facebook', ['ngRoute','ngFacebook'])
     }
    
     function refresh(){
-        $facebook.api("/me?fields=name,email").then(function(response){
+        $facebook.api("/1869607480026851").then(function(response){
             $scope.welcomeMsg = 'Welcome '+response.name;
             $scope.isLoggedIn = true;
             $scope.userInfo = response;
