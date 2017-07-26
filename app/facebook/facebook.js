9a02b3e94f6c17cdbaa850e9567c934191515e82
Pogrebnyak Alex <pogrebnyakalexx@gmail.com>
2017-07-26 17:43:19 +0300

    Add additional log to postStatus()

--- app/facebook/facebook.js
+++ app/facebook/facebook.js
@@ -67,6 +67,8 @@ angular.module('ngSocial.facebook', ['ngRoute','ngFacebook'])
     
     $scope.postStatus = function(){
         var body = $scope.body;
+        console.log('We are in postStatus function before $facebook.api');
+        console.log('body is '+body);
         $facebook.api('me/feed','post',{message:body}).then(function(response){
             $scope.msg = 'Thanks for posting';
             console.log(response);
